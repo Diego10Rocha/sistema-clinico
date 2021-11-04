c6f033b666a1f57b3e656424abc2d0bb2c865b64
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-04 09:47:09 -0300

    Add método equals class Especialidade

--- src/main/java/model/Especialidade.java
+++ src/main/java/model/Especialidade.java
@@ -3,27 +3,52 @@ package model;
 import dao.EspecialidadeDAO;
 
 public class Especialidade {
-	
+
 	private String nome;
-	private boolean principal;//true - principal; false - subespecialidade
-	
+	private boolean principal;// true - principal; false - subespecialidade
+
 	public Especialidade(String nome, boolean principal) {
 		this.nome = nome;
 		this.principal = principal;
 		EspecialidadeDAO.insertSpecialty(this);
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public boolean isPrincipal() {
 		return principal;
 	}
+
 	public void setPrincipal(boolean principal) {
 		this.principal = principal;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		boolean objIsEqual = false;
+
+		if (obj instanceof Especialidade) {
+
+			Especialidade especialidadeASerComparada = (Especialidade) obj;
+
+			String nomeEspecialidadeASerComparada = especialidadeASerComparada.getNome();
+			boolean principalEspecialidadeASerComparada = especialidadeASerComparada.isPrincipal();
+
+			if (nomeEspecialidadeASerComparada.equals(this.getNome())
+					&& principalEspecialidadeASerComparada == this.isPrincipal())
+				
+				objIsEqual = true;
+
+		}
+
+		return objIsEqual;
 	}
 
 }
