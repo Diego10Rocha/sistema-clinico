4cde09e3719169a8855ccf2664a14a3519950b46
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-10 20:02:51 -0300

    Add jfoenix

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class LoginController {
+
+	@FXML
+	private JFXTextField txtCPF;
+
+	@FXML
+	private JFXPasswordField txtSenha;
+
+	@FXML
+	private JFXButton btnEntrar;
+
+	@FXML
+	void makeLogin(ActionEvent event) {
+
+	}
+
+}
