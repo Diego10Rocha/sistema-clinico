2a2f5cf9dd8b751e91f2b4436e7036e7304e3ad1
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-11 08:07:47 -0300

    update screen Login

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -3,23 +3,23 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController {
 
 	@FXML
-    private TextField txtCPF;
+	private TextField txtCPF;
 
-    @FXML
-    private TextField txtSenha;
+	@FXML
+	private PasswordField txtSenha;
 
-    @FXML
-    private Button btnEntrar;
+	@FXML
+	private Button btnEntrar;
 
-    @FXML
-    void makeLogin(ActionEvent event) {
+	@FXML
+	void makeLogin(ActionEvent event) {
 
-    }
-
+	}
 
 }
