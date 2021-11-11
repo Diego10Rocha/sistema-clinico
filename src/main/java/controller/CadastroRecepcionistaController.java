a50fd3a96e97f23fda010b33de51efa319800ae9
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-11 08:19:53 -0300

    Add controller FormularioCadastroRecepcionista

---
+++ src/main/java/controller/CadastroRecepcionistaController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CadastroRecepcionistaController {
+
+	@FXML
+	private TextField txtCPF;
+
+	@FXML
+	private PasswordField txtSenha;
+
+	@FXML
+	private Button btnEntrar;
+
+	@FXML
+	private TextField txtNome;
+
+	@FXML
+	void cadastrarRecepcionista(ActionEvent event) {
+
+	}
+
+}
