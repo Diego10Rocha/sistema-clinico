d76cead1451a4ce6f95f05c2e4d55afa7555f30a
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-11 09:23:34 -0300

    Add Implementation method cadastrarRecepcionista

--- src/main/java/controller/CadastroRecepcionistaController.java
+++ src/main/java/controller/CadastroRecepcionistaController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import message.MessageAlert;
 import model.Recepcionista;
 
 public class CadastroRecepcionistaController {
@@ -22,25 +23,52 @@ public class CadastroRecepcionistaController {
 	@FXML
 	private TextField txtNome;
 
+	private MessageAlert msgAlert = new MessageAlert();
+
 	@FXML
 	void cadastrarRecepcionista(ActionEvent event) {
-		
-		String CPF = txtCPF.getText();
-		
-		boolean isCpfAlreadyRegistered = RecepcionistaDAO.cpfAlreadyRegistered(CPF);
-		
-		if(isCpfAlreadyRegistered) {
-			
-			System.out.println("CPF já cadastrado");
+
+		boolean isAnyCampoEmBranco = isAnyCampoEmBranco();
+
+		if (isAnyCampoEmBranco) {
+
+			msgAlert.getMessageCadastroRecepcionistaFailCampoEmBranco();
 		}
-		
+
 		else {
 			
-			String name = txtNome.getText();
-			String password = txtSenha.getText();
-			
-			Recepcionista newRecepcionista = new Recepcionista(name, CPF, password);
+			String CPF = txtCPF.getText();
+
+			boolean isCpfAlreadyRegistered = RecepcionistaDAO.cpfAlreadyRegistered(CPF);
+
+			if (isCpfAlreadyRegistered) {
+
+				msgAlert.getMessageCadastroRecepcionistaFailCpfAlreadyRegistered();
+			}
+
+			else {
+
+				String name = txtNome.getText();
+				String password = txtSenha.getText();
+
+				Recepcionista newRecepcionista = new Recepcionista(name, CPF, password);
+				
+				msgAlert.getMessageCadastroRecepcionistaSucces();
+			}
 		}
+
+	}
+
+	private boolean isAnyCampoEmBranco() {
+
+		boolean anyCampoEmBranco = false;
+
+		if (txtCPF.getText().equals("") || txtNome.getText().equals(" ") || txtSenha.getText().equals("")) {
+
+			anyCampoEmBranco = true;
+		}
+
+		return anyCampoEmBranco;
 	}
 
 }
--- src/main/java/message/MessageAlert.java
+++ src/main/java/message/MessageAlert.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
 Autor: Diego Cerqueira e Joanderson Santos
-Componente Curricular: MI Programação
+Componente Curricular: MI Programaï¿½ï¿½o
 Concluido em: 18/10/2021
-Declaro que este código foi elaborado por Diego Cerqueira e Joanderson Santos em dupla e não contém nenhum
-trecho de código de outro colega ou de outro autor, tais como provindos de livros e
-apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
-de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
-do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+Declaro que este cï¿½digo foi elaborado por Diego Cerqueira e Joanderson Santos em dupla e nï¿½o contï¿½m nenhum
+trecho de cï¿½digo de outro colega ou de outro autor, tais como provindos de livros e
+apostilas, e pï¿½ginas ou documentos eletrï¿½nicos da Internet. Qualquer trecho de cï¿½digo
+de outra autoria que nï¿½o a minha estï¿½ destacado com uma citaï¿½ï¿½o para o autor e a fonte
+do cï¿½digo, e estou ciente que estes trechos nï¿½o serï¿½o considerados para fins de avaliaï¿½ï¿½o.
 ******************************************************************************************/
 
 package message;
@@ -15,149 +15,168 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 /**
- * A Classe <b>MessageAlert</b> representa a modelagem de <b>mensagens</b> de <b>alerta</b> 
- * referente à GUI do <b>Gerenciador de Tarefas</b>.
+ * A Classe <b>MessageAlert</b> representa a modelagem de <b>mensagens</b> de
+ * <b>alerta</b> referente ï¿½ GUI do <b>Gerenciador de Tarefas</b>.
+ * 
  * @since 2021
  * @author Joanderson Santos e Dicego Cerqueira
  */
 public class MessageAlert {
-	
+
 	private Alert alert;
-	
+
 	/**
 	 * Apresenta uma <b>mensagem</b> de aviso na tela.<br>
-	 * Conteúdo da mensagem: "Por favor preencha os campos primeiro".
+	 * Conteï¿½do da mensagem: "Por favor preencha os campos primeiro".
 	 */
-	
-	public void getMessageCampoEmBranco() {
-		
-		alert = new Alert(AlertType.WARNING);
-		alert.setContentText("Por favor preencha os campos primeiro");
-		
+
+	public void getMessageCadastroRecepcionistaSucces() {
+
+		alert = new Alert(AlertType.CONFIRMATION);
+		alert.setContentText("Cadastro Realizado com sucesso");
+
 		alert.show();
-		
+
 	}
 	
+	public void getMessageCadastroRecepcionistaFailCampoEmBranco() {
+
+		alert = new Alert(AlertType.WARNING);
+		alert.setContentText("Por Favor preencha todos os campos!");
+
+		alert.show();
+
+	}
+	
+	public void getMessageCadastroRecepcionistaFailCpfAlreadyRegistered() { 
+
+		alert = new Alert(AlertType.WARNING);
+		alert.setContentText("CPF jÃ¡ Cadastrado!");
+
+		alert.show();
+
+	}
+
 	/**
 	 * Apresenta uma <b>mensagem</b> de aviso na tela.<br>
-	 * Conteúdo da mensagem: "As tarefas devem estar concluídas para que um projeto seja excluído".
+	 * Conteï¿½do da mensagem: "As tarefas devem estar concluï¿½das para que um projeto
+	 * seja excluï¿½do".
 	 */
-	
+
 	public void getMessageTarefasNaoConcluidas() {
-		
+
 		alert = new Alert(AlertType.WARNING);
-		alert.setContentText("As tarefas devem estar concluídas para que um projeto seja excluído");
-		
+		alert.setContentText("As tarefas devem estar concluï¿½das para que um projeto seja excluï¿½do");
+
 		alert.show();
-		
+
 	}
-	
+
 	/**
-	 * Apresenta uma <b>mensagem</b> de confirmação na tela.<br>
-	 * Conteúdo da mensagem: "Projeto Salvo com Sucesso".
+	 * Apresenta uma <b>mensagem</b> de confirmaï¿½ï¿½o na tela.<br>
+	 * Conteï¿½do da mensagem: "Projeto Salvo com Sucesso".
 	 */
-	
+
 	public void getMessageProjetoSalvo() {
-		
+
 		alert = new Alert(AlertType.CONFIRMATION);
 		alert.setContentText("Projeto Salvo com Sucesso!");
-		
+
 		alert.show();
 	}
-	
+
 	/**
-	 * Apresenta uma <b>mensagem</b> de confirmação na tela.<br>
-	 * Conteúdo da mensagem: "Projeto Editado com Sucesso".
+	 * Apresenta uma <b>mensagem</b> de confirmaï¿½ï¿½o na tela.<br>
+	 * Conteï¿½do da mensagem: "Projeto Editado com Sucesso".
 	 */
-	
+
 	public void getMessageProjetoEditado() {
-		
+
 		alert = new Alert(AlertType.CONFIRMATION);
 		alert.setContentText("Projeto Editado com Sucesso!");
-		
+
 		alert.show();
 	}
-	
+
 	/**
 	 * Apresenta uma <b>mensagem</b> de aviso na tela.<br>
-	 * Conteúdo da mensagem: "Por favor selecione um projeto primeiro!".
+	 * Conteï¿½do da mensagem: "Por favor selecione um projeto primeiro!".
 	 */
-	
+
 	public void getMessageProjetoNaoSelecionada() {
-		
+
 		alert = new Alert(AlertType.WARNING);
 		alert.setContentText("Por favor selecione um projeto primeiro!");
-		
+
 		alert.show();
 	}
-	
+
 	/**
-	 * Apresenta uma <b>mensagem</b> de confirmação na tela.<br>
-	 * Conteúdo da mensagem: "Projeto excluído com Sucesso!".
+	 * Apresenta uma <b>mensagem</b> de confirmaï¿½ï¿½o na tela.<br>
+	 * Conteï¿½do da mensagem: "Projeto excluï¿½do com Sucesso!".
 	 */
-	
+
 	public void getMessageProjetoExcluida() {
-		
+
 		alert = new Alert(AlertType.CONFIRMATION);
-		alert.setContentText("Projeto excluído com Sucesso!");
-		
+		alert.setContentText("Projeto excluï¿½do com Sucesso!");
+
 		alert.show();
 	}
-	
+
 	/**
-	 * Apresenta uma <b>mensagem</b> de confirmação na tela.<br>
-	 * Conteúdo da mensagem: "Tarefa salva com Sucesso!".
+	 * Apresenta uma <b>mensagem</b> de confirmaï¿½ï¿½o na tela.<br>
+	 * Conteï¿½do da mensagem: "Tarefa salva com Sucesso!".
 	 */
 
 	public void getMessageTarefaSalva() {
-		
+
 		alert = new Alert(AlertType.CONFIRMATION);
 		alert.setContentText("Tarefa salva com Sucesso!");
-		
+
 		alert.show();
-		
+
 	}
-	
+
 	/**
-	 * Apresenta uma <b>mensagem</b> de confirmação na tela.<br>
-	 * Conteúdo da mensagem: "Tarefa editada com Sucesso!".
+	 * Apresenta uma <b>mensagem</b> de confirmaï¿½ï¿½o na tela.<br>
+	 * Conteï¿½do da mensagem: "Tarefa editada com Sucesso!".
 	 */
 
 	public void getMessageTarefaEditada() {
-		
+
 		alert = new Alert(AlertType.CONFIRMATION);
 		alert.setContentText("Tarefa editada com Sucesso!");
-		
+
 		alert.show();
-		
+
 	}
-	
+
 	/**
 	 * Apresenta uma <b>mensagem</b> de aviso na tela.<br>
-	 * Conteúdo da mensagem: "Por favor selecione uma Tarefa!".
+	 * Conteï¿½do da mensagem: "Por favor selecione uma Tarefa!".
 	 */
 
 	public void getMessageTarefaNaoSelecionada() {
-		
+
 		alert = new Alert(AlertType.WARNING);
 		alert.setContentText("Por favor selecione uma Tarefa!");
-		
+
 		alert.show();
-		
+
 	}
-	
+
 	/**
-	 * Apresenta uma <b>mensagem</b> de confirmação na tela.<br>
-	 * Conteúdo da mensagem: "Tarefa excluída com Sucesso!".
+	 * Apresenta uma <b>mensagem</b> de confirmaï¿½ï¿½o na tela.<br>
+	 * Conteï¿½do da mensagem: "Tarefa excluï¿½da com Sucesso!".
 	 */
-	
+
 	public void getMessageTarefaExcluida() {
-	
-		
+
 		alert = new Alert(AlertType.CONFIRMATION);
-		alert.setContentText("Tarefa excluída com Sucesso!");
-		
+		alert.setContentText("Tarefa excluï¿½da com Sucesso!");
+
 		alert.show();
 	}
-		
+
 }
