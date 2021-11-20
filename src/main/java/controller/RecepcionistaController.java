b935dbe795b01a4e78045ab2b6cb0c4f976e653c
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-20 08:44:17 -0300

    Add class RecepcionistaController

--- src/main/java/controller/RecepcionistaController.java
+++ src/main/java/controller/RecepcionistaController.java
@@ -1,5 +1,74 @@
 package controller;
 
 public class RecepcionistaController {
+	@FXML
+	private Menu menuCadastro;
+
+	@FXML
+	private MenuItem menuItemCadastroRecepcionista;
+
+	@FXML
+	private MenuItem menuItemCadastroMedico;
+
+	@FXML
+	private MenuItem menuItemCadastroPaciente;
+
+	@FXML
+	private Menu menuConsulta;
+
+	@FXML
+	private MenuItem menuItemAddConsulta;
+
+	@FXML
+	private MenuItem menuItemVerConsultas;
+
+	@FXML
+	private Menu menuMedicos;
+
+	@FXML
+	private Menu menuEspecialidades;
+
+	@FXML
+	private Menu menuSair;
+
+	@FXML
+	void openScreenAddConsulta(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenCadastroMedico(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenCadastroPaciente(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenCadastroRecepcionista(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenConsultas(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenEspecialidades(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenMedicosCadastrados(ActionEvent event) {
+
+	}
+
+	@FXML
+	void sair(ActionEvent event) {
+
+	}
 
 }
