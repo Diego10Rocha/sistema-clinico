385bf78713acff7c08c53ebd07d27834390580c9
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-20 11:53:12 -0300

    Add class EspecialidadeController

---
+++ src/main/java/controller/EspecialidadeController.java
@@ -0,0 +1,38 @@
+package controller;
+
+public class EspecialidadeController {
+	@FXML
+	private ListView<?> lvConsultas;
+
+	@FXML
+	private Button btnAdicionarEspecialidade;
+
+	@FXML
+	private Button btnRemoverEspecialidade;
+
+	@FXML
+	private Button btnEditarEspecialidade;
+
+	@FXML
+	private Button btnVoltar;
+
+	@FXML
+	void closeScreen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenFormularioEditEspecialidade(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenFormularioEspecialidade(ActionEvent event) {
+
+	}
+
+	@FXML
+	void removerEspecialidade(ActionEvent event) {
+
+	}
+}
