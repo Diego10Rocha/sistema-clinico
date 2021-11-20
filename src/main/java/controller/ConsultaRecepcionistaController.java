ba181b119b0047b25a2b52acb4ac1cc99ee030b1
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-20 11:45:40 -0300

    Add class ConsultaRecepcionistaController

---
+++ src/main/java/controller/ConsultaRecepcionistaController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class ConsultaRecepcionistaController {
+	
+	@FXML
+	private ListView<?> lvConsultas;
+
+	@FXML
+	private Button btnMarcarConsulta;
+
+	@FXML
+	private Button btnAdicionarConsulta;
+
+	@FXML
+	private Button btnRemoverConsulta;
+
+	@FXML
+	private Button btnEditarConsulta;
+
+	@FXML
+	private Button btnVoltar;
+
+	@FXML
+	void closeScreen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void marcarConsulta(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenFormularioConsulta(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenFormularioEditConsulta(ActionEvent event) {
+
+	}
+
+	@FXML
+	void removerConsulta(ActionEvent event) {
+
+	}
+}
