dc86648e22eb77ae213b5bb3238d620d41ccf380
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-21 12:22:48 -0300

    Rename

--- src/main/java/controller/AutoAtendimentoController.java
+++ src/main/java/controller/AutoAtendimentoController.java
@@ -1,21 +1,49 @@
 package controller;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import dao.AgendaConsultaDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import model.AgendaConsulta;
 
-public class AutoAtendimentoController {
+public class AutoAtendimentoController implements Initializable{
 
 	@FXML
-    private ListView<Object> lvConsultas;
+	private ListView<AgendaConsulta> lvConsultas;
 
-    @FXML
-    private Button btnMarcarConsulta;
+	@FXML
+	private Button btnMarcarConsulta;
+	
+	private ObservableList<AgendaConsulta> obsConsultas;
 
-    @FXML
-    void openScreenLogin(ActionEvent event) {
 
-    }
+	@FXML
+	void openScreenLogin(ActionEvent event) {
+
+	}
+
+	private void loadConsultas() {
+
+		List<AgendaConsulta> agendasConsultaCadastradas = AgendaConsultaDAO.getAgendasConsulta();
+		obsConsultas = FXCollections.observableArrayList(agendasConsultaCadastradas);
+
+		lvConsultas.setItems(obsConsultas);
+
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		loadConsultas();
+		
+	}
 
 }
