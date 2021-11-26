aa1680b59999f2f26679c3a42caee3b2b7901a17
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-11-26 17:03:21 -0300

    Add package and class UserFactory

---
+++ src/main/java/Factory/UserFactory.java
@@ -0,0 +1,26 @@
+package Factory;
+
+import model.Medico;
+import model.Paciente;
+import model.Recepcionista;
+
+public class UserFactory {
+
+	public static void createMedico(String name, String password, String CPF, String CRM, String especialidade,
+			String subEspecialidade, String horaDisponivelConsulta) {
+
+		Medico newMedico = new Medico(name, password, CPF, CRM, especialidade, subEspecialidade,
+				horaDisponivelConsulta);
+	}
+
+	public static void createPaciente(String name, String CPF, String dataNascimento) {
+
+		Paciente newPaciente = new Paciente(name, CPF, dataNascimento);
+	}
+
+	public static void createRecepcionista(String name, String CPF, String password) {
+
+		Recepcionista newRecepcionista = new Recepcionista(name, CPF, password);
+
+	}
+}
