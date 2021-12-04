43b6fee757479919aee4fdbb0f26fc028c81ab54
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-12-04 14:04:07 -0300

    Add class AtendimentoMedicoController

---
+++ src/main/java/controller/consulta/AtendimentoMedicoController.java
@@ -0,0 +1,46 @@
+package controller.consulta;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class AtendimentoMedicoController {
+
+	@FXML
+	private TextArea txtAnamnese;
+
+	@FXML
+	private Button btnSalvar;
+
+	@FXML
+	private Button btnReceita;
+
+	@FXML
+	private Button btnVoltar;
+
+	@FXML
+	private TextArea txtExameFisico;
+
+	@FXML
+	private TextArea txtDiagnosticosDefenitivos;
+
+	@FXML
+	private TextArea txtTratamentosEfetuados;
+
+	@FXML
+	void closeScreen(ActionEvent event) {
+
+	}
+
+	@FXML
+	void encerrarConsulta(ActionEvent event) {
+
+	}
+
+	@FXML
+	void openScreenReceita(ActionEvent event) {
+
+	}
+
+}
