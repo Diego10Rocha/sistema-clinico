d1cb0e0b8aa712685f31f9b8cb499c7cb4a5f132
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-12-05 10:00:37 -0300

    Update ConsultaMedicoController

--- src/main/java/controller/consulta/ConsultaMedicoController.java
+++ src/main/java/controller/consulta/ConsultaMedicoController.java
@@ -35,6 +35,8 @@ public class ConsultaMedicoController implements Initializable, EventHandler<Act
 	private Button btnAtenderPaciente;
 	@FXML
 	private Button btnCancelarConsulta;
+	@FXML
+	private Button btnPacientes;
 
 	@FXML
 	private Button btnVoltar;
@@ -107,6 +109,12 @@ public class ConsultaMedicoController implements Initializable, EventHandler<Act
 	void closeScreen(ActionEvent event) {
 
 		ScreenManager.closeScreen(btnVoltar);
+	}
+
+	@FXML
+	void openScreenMedicoPacientes(ActionEvent event) throws IOException {
+
+		screenManager.openNewScreen("medico/MedicoPacientes", "Pacientes");
 	}
 
 	@Override
