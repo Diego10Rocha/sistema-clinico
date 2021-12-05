46bebd5eb6f172151461a8155a1d090a6773d2e7
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-12-05 10:13:11 -0300

    Add screen MedicoPacientesController

---
+++ src/main/java/controller/medico/MedicoPacientesController.java
@@ -0,0 +1,82 @@
+package controller.medico;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import dao.MedicoDAO;
+import dao.PacienteDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
+import login.Login;
+import message.MessageAlert;
+import model.Medico;
+import model.Paciente;
+import screenManager.ScreenManager;
+
+public class MedicoPacientesController implements Initializable {
+
+	@FXML
+	private ListView<Paciente> lvPacientes;
+
+	@FXML
+	private Button btnProntuarios;
+
+	@FXML
+	private Button btnVoltar;
+
+	private Medico medicoQueDetemPacientes;
+	private ObservableList<Paciente> obsPacientes;
+	private ScreenManager screenManager = new ScreenManager();
+
+	@FXML
+	void closeScreen(ActionEvent event) {
+
+		ScreenManager.closeScreen(btnVoltar);
+	}
+
+	@FXML
+	void openScreenProntuariosPacientes(ActionEvent event) {
+
+		Paciente pacienteSelecionado = lvPacientes.getSelectionModel().getSelectedItem();
+
+		if (pacienteSelecionado == null) {
+
+			new MessageAlert().showMessage("Por Favor selecione um Paciente primeiro!", AlertType.WARNING);
+
+		}
+
+		else {
+
+			screenManager.openNewScreen("paciente/ProntuariosPaciente", "Prontuarios");
+
+			setReferenciaFormularioPacienteEdit();
+
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		medicoQueDetemPacientes = MedicoDAO.findByCPF(Login.getCPF_userLogged());
+
+		loadPacientes();
+
+	}
+
+	private void loadPacientes() {
+
+		List<Paciente> medicoPacientes = medicoQueDetemPacientes.getPacientes();
+
+		obsPacientes = FXCollections.observableArrayList(medicoPacientes);
+
+		lvPacientes.setItems(obsPacientes);
+	}
+
+}
